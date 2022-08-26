bfc3cda297805ab2f2746c51ffb8f8f3e762728f
koechronix <koechronix@yahoo.com>
2022-08-26 13:21:32 -0400

    completed

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters, dependent: :destroy
+    has_many :shows, through: :characters
+
+    def full_name
+        "#{first_name} #{last_name}"
+    end
+
+    def list_roles
+        self.characters.map do |character|
+            "#{character[:name]} - #{Show.find_by(id: (character[:show_id])).name}"
+        end
+    end
 end
--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,10 @@
 class Network < ActiveRecord::Base
+    has_many :shows, dependent: :destroy
+
+    def sorry
+        if call_letters.downcase == "nbc"
+            "We're sorry about passing on John Mulaney's pilot"
+        end
+    end
   
 end
